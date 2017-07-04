d4ff6deb876a77921e2bee6415e201c59b24f215
Ranjangsk <gskranjan@gmail.com>
2017-07-04 15:04:34 +0530

    add new js file

--- server/server.js
+++ server/server.js
@@ -1,12 +1,52 @@
 const path = require('path');
 const express = require('express');
+const socketIO=require('socket.io');
+const http=require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server=http.createServer(app);
+
+var io=socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+
+/*io.on('connection',(socket)=>{
+    console.log('new user connected');
+    
+ 
+    
+    socket.on('disconnect',()=>{
+        console.log('disconnected');
+    })
+});
+*/
+io.on('connection',(socket)=>{
+    
+    console.log('new user connected');
+    
+    socket.emit('newMessage',{
+        from:'ranjan',
+        text:'hey',
+        createdAt:123123
+        
+    });
+    
+    
+    socket.on('createMessage',function(message){
+        console.log(message);
+    })
+    
+    socket.on('disconnect',()=>{
+        
+        console.log('client is disconnected');
+    });
+    
+})
+
+
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
