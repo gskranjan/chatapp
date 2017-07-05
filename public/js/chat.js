1dbdadc09a59e6262a3c36ce109dff26c77ac4c4
Ranjangsk <gskranjan@gmail.com>
2017-07-05 21:46:20 +0530

    added join page

--- public/js/chat.js
+++ public/js/chat.js
@@ -27,6 +27,20 @@ socket.on('disconnect',function(){
 */
 var socket=io();
 
+function scrollToBottom () {
+  
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
 
 socket.on('connect',function(){
     console.log('connected to server');
@@ -47,7 +61,7 @@ socket.on('connect',function(){
         
         jQuery('#messages').append(html);
         
-      
+      scrollToBottom();
       
         
       //    var li=jQuery('<li></li>');
@@ -143,7 +157,7 @@ socket.on('connect',function(){
             });
             
         },function(){
-             locationButton.removeAttr('disabled').text('Send location');
+            
             return alert('unable to fetch location');
         })
         
